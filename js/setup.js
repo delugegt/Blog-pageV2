3cc610f465709eb3d0cdc89df297dea6bedf9cfa
delugegt <delugegt@hotmail.com>
2021-09-16 19:20:19 -0300

    commit de configuração principal, carousel

---
+++ js/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:0,
+  nav:false,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          /*itens por vez*/
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+});
